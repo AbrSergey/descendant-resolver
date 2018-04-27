4c245b576de071cc033fc885a6e1d0b1cc82d769
Sergey <sergey.abramov996.com>
2018-04-27 15:50:31 +0700

    Add next classes: neterminal, L1, L2

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
+# define class of neterminal
+class neterminal:
+    def __init__(self, neterm, count_alt, first_pos):  # neterminal, count alternatives, position in list
+        self.n = neterm
+        self.c = count_alt
+        self.f = first_pos
 
+# define class of stack L1
+class L1:
+    def __init__(self, flag, symbol, number_alt, count_alt):  # flag terminal/neterminal, symbol,
+        # number current alternative, count of alternatives
+        # if neterminal then number_alt and count_alt don't use
+        self.f = flag
+        self.s = symbol
+        self.n = number_alt
+        self.c = count_alt
+
+# define class of stack L2
+class L2:
+    def __init__(self, flag, symbol):
+        self.f = flag
+        self.s = symbol
 
 def main():
+    # initialzation
     str = "!a+b!"
     LR = "ABBTTMMM"
-    RR = {"!B!", "T+B", "T", "M", "M*T", "a", "b", "(B)"}
-    m = 4 // number of non-terminals
+    RR = ["!B!", "T+B", "T", "M", "M*T", "a", "b", "(B)"]
+    m = 4 # number of non-terminals
+    sos # q, b, t
+    T = "!+*ab()" # set of terminals
 
-    s = "q" // q, b, t
-    L1 = [] // stack
-    L2 = [] // stack
-    w = str
-    steps = ""
+    A = neterminal('A', 1, 0)
+    B = neterminal('B', 2, 1)
+    C = neterminal('C', 2, 3)
+    M = neterminal('M', 3, 5)
 
-    // start
-    
+    S= 'A'  # initial character
+
+if __name__ == '__main__':
+    main()
